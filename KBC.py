185af25c56f9fbfcba8ea1623dfe3dd8429380c4
Shruti-Chavan-1008 <shrutichavan1975@gmail.com>
2026-06-25 12:52:35 +0530

    reate KBC quiz game with 10 questions, answer validation and prize amount system

---
+++ KBC.py
@@ -0,0 +1,85 @@
+questions = [
+    "What is the capital of India?",
+    "Which planet is known as the Red Planet?",
+    "Who is known as the Father of Computers?",
+    "Which is the largest ocean in the world?",
+    "How many players are there in one cricket team?",
+    "Which gas do plants absorb during photosynthesis?",
+    "Who wrote the Indian National Anthem?",
+    "Which programming language is mostly used for AI and Data Science?",
+    "Which is the fastest land animal?",
+    "Who is the founder of Microsoft?"
+]
+
+
+options = [
+    ["A) Mumbai", "B) New Delhi", "C) Kolkata", "D) Chennai"],
+
+    ["A) Earth", "B) Jupiter", "C) Mars", "D) Venus"],
+
+    ["A) Charles Babbage", "B) Alan Turing", "C) Bill Gates", "D) Steve Jobs"],
+
+    ["A) Atlantic Ocean", "B) Indian Ocean", "C) Pacific Ocean", "D) Arctic Ocean"],
+
+    ["A) 9", "B) 10", "C) 11", "D) 12"],
+
+    ["A) Oxygen", "B) Carbon Dioxide", "C) Nitrogen", "D) Hydrogen"],
+
+    ["A) Mahatma Gandhi", "B) Rabindranath Tagore",
+     "C) Jawaharlal Nehru", "D) Subhash Chandra Bose"],
+
+    ["A) HTML", "B) Python", "C) CSS", "D) XML"],
+
+    ["A) Lion", "B) Tiger", "C) Cheetah", "D) Horse"],
+
+    ["A) Elon Musk", "B) Bill Gates",
+     "C) Mark Zuckerberg", "D) Jeff Bezos"]
+]
+
+
+answers = [
+    "B",
+    "C",
+    "A",
+    "C",
+    "C",
+    "B",
+    "B",
+    "B",
+    "C",
+    "B"
+]
+
+
+money = 0
+
+
+print("🔥 Welcome to KBC Quiz Game 🔥")
+
+
+for i in range(len(questions)):
+
+    print("--------------------------------")
+    print("Q", i+1, questions[i])
+
+    for option in options[i]:
+        print(option)
+
+    user_answer = input("Enter your answer (A/B/C/D): ")
+
+    if user_answer.upper() == answers[i]:
+
+        money = money + 500
+
+        print("Correct Answer 🎉")
+        print("You won ₹", money)
+
+    else:
+
+        print("Wrong Answer ❌")
+        print("Game Over")
+        break
+
+
+print("--------------------------------")
+print("Your total winning amount is ₹", money)
