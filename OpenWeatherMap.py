79d5d85fae01e13ab5a38e3b790669d5875fa79b
Shruti-Chavan-1008 <shrutichavan1975@gmail.com>
2026-07-11 17:47:07 +0530

    Build OpenWeatherMap data search using Pandas

---
+++ OpenWeatherMap.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("globalAirQuality.csv")
+ 
+print("----------------------------------")
+print("OpenWeatherMap")
+print("----------------------------------")
+print("Check The weather info Of Your city")
+print("----------------------------------")
+
+Country=input("Enter the country name in short From : ").upper()
+City=input("Enter the city name of that paticular country:  ")
+
+
+result = df[(df["country"] == Country) & (df["city"] == City)]
+ 
+
+ 
+if result.empty:
+    print("Country not found")
+else:
+    print(f"Country: {result.iloc[0]['country']}")
+    print(f"City: {result.iloc[0]['city']}")
+    print(f"AQI: {result.iloc[0]['aqi']}")
+    print(f"Humidity: {result.iloc[0]['humidity']}")
+    print(f"Wind Speed: {result.iloc[0]['wind_speed']}")
+    print(f"Temperature: {result.iloc[0]['temperature']}")
+
+print("----------------------------------")
+print("Thank you for visting ")
+print("----------------------------------")
